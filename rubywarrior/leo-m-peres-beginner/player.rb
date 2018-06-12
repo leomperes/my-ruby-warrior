fe486488f9a6c69c9b3f92bf798fefd87b75a214
Leo M. Peres <leomperes@belanton.com>
2018-06-12 17:22:04 -0400

    Complete level 6

--- rubywarrior/leo-m-peres-beginner/player.rb
+++ rubywarrior/leo-m-peres-beginner/player.rb
@@ -3,30 +3,67 @@ require 'pry'
 class Player
   # Constants declarations:
   MAX_HEALTH = 20
+  HEALTHY = 16
+  HALF_HEALTH = 10
+  MIN_HEALTH = 7
 
   # Initialize instance variables.
   def initialize
     @health = MAX_HEALTH
+    @direction = :forward
   end
 
   def play_turn(warrior)
 
+    # Check if the warrior is under archer attack.
     archer_attack = warrior.health < @health
 
-    if warrior.feel.empty?
+    if warrior.feel(@direction).empty? || warrior.feel(:backward).wall?
 
       # Keep the warrior health up.
-      if warrior.health >= MAX_HEALTH || archer_attack
-        warrior.walk!
+      if warrior.health >= HEALTHY || archer_attack
+        warrior.walk! @direction
+        @direction = :forward if warrior.feel(:backward).wall?
       else
         warrior.rest!
       end
 
     else
-      warrior.feel.captive? ? warrior.rescue! : warrior.attack!
+
+      rescue_or_attack(warrior)
+
     end
     # binding.pry
 
     @health = warrior.health
+  end
+
+  private
+
+  # Use +warrior.feel.captive?+ to see if there is a captive and
+  # +warrior.rescue!+ to rescue him. Don't attack captives.
+  def rescue_or_attack(warrior)
+    if warrior.feel(@direction).captive?
+      warrior.rescue! @direction
+      @direction = :forward
+    else
+      warrior.attack!
+      @direction = :backward if need_rest?(warrior)
+    end
+  end
+
+  def need_rest?(warrior)
+    warrior.health <= HALF_HEALTH
+  end
+
+  # Prints a log of variables on console.
+  def console_log(warrior)
+    puts "@health = #{@health}"
+    puts "@direction = #{@direction}"
+    puts "warrior.health = #{warrior.health}"
+    puts "warrior.feel = #{warrior.feel}"
+    puts "warrior.feel(:#{@direction}) = #{warrior.feel(@direction)}"
+    puts "warrior.feel.empty? = #{warrior.feel.empty?}"
+    print "warrior.feel.captive? = #{warrior.feel.captive?}\n\n"
   end
 end
