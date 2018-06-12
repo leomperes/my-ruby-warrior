d13ff34fd7069a017f601e8349daa7e5d69435d8
Leo M. Peres <leomperes@belanton.com>
2018-06-12 14:53:56 -0400

    Complete level 4

--- rubywarrior/leo-m-peres-beginner/player.rb
+++ rubywarrior/leo-m-peres-beginner/player.rb
@@ -1,11 +1,31 @@
+require 'pry'
+
 class Player
+  # Constants declarations:
   MAX_HEALTH = 20
 
+  # Initialize instance variables.
+  def initialize
+    @health = MAX_HEALTH
+  end
+
   def play_turn(warrior)
+
+    archer_attack = warrior.health < @health
+
     if warrior.feel.empty?
-      warrior.health < MAX_HEALTH ? warrior.rest! : warrior.walk!
+
+      if warrior.health >= MAX_HEALTH || archer_attack
+        warrior.walk!
+      else
+        warrior.rest!
+      end
+
     else
       warrior.attack!
     end
+    # binding.pry
+
+    @health = warrior.health
   end
 end
